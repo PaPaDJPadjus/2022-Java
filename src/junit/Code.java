51b3524a0372b5abb3daab2e79c6caa60722e1cd
Patrick Padjus <papadj@ttu.ee>
2023-02-20 10:45:22 +0200

    ex03 done attempt 1

---
+++ src/junit/Code.java
@@ -0,0 +1,116 @@
+package junit;
+
+
+import java.util.Arrays;
+
+public class Code {
+
+    public static void main(String[] args) {
+        System.out.println(sumIgnoringDuplicates(new int[]{1, 1}));
+
+    }
+
+    public static boolean isSpecial(int candidate) {
+        return candidate % 11 == 0 || candidate % 11 == 1 || candidate % 11 == 2 || candidate % 11 == 3;
+    }
+
+    public static int longestStreak(String inputString) {
+        if (inputString.equals("")) {
+            return 0;
+        }
+        if (inputString.length() == 1) {
+            return 1;
+        }
+
+        int record = 0;
+        int streakCounter = 1;
+        for (int i = 0; i <= inputString.length() - 1; i++) {
+            char charFromString = inputString.charAt(i);
+            for (int j = 1; j <= inputString.length() - i; j++) {
+                char charsAfterSearchableChar = inputString.charAt(i + j);
+                if (charFromString == charsAfterSearchableChar) {
+                    streakCounter += 1;
+                }
+            }
+            if (streakCounter > record) {
+                record = streakCounter;
+            }
+            streakCounter = 0;
+        }
+        return record;
+    }
+
+    public static Character mode(String input) {
+        if (input == null || input.equals("")) {
+            return null;
+        }
+        int mostComCharCounter = 1;
+        Character mostCommonChar = 'x';
+        for (int i = 0; i <= input.length() - 1; i++) {
+            int newCharAmount = getCharacterCount(input, input.charAt(i));
+            if (newCharAmount > mostComCharCounter) {
+                mostCommonChar = input.charAt(i);
+            }
+        }
+        return mostCommonChar;
+    }
+
+    public static int getCharacterCount(String allCharacters, char targetCharacter) {
+
+        int currentCharCounter = 1;
+        for (int searchableChar = 1; searchableChar < allCharacters.length(); searchableChar++) {
+
+            if (allCharacters.charAt(searchableChar) == targetCharacter) {
+                currentCharCounter += 1;
+            }
+        }
+        return currentCharCounter;
+    }
+
+    public static int integerInArrayCount(int integer, int[] integersArray) {
+        int counter = 0;
+        for (int el : integersArray) {
+            if (integer == el) {
+                counter += 1;
+            }
+        }
+        return counter;
+    }
+
+    public static int[] addNewSlotWithElToArray(int element, int[] integers) {
+        int[] outputArray = new int[integers.length + 1];
+        int index = 0;
+        for (int el : integers) {
+            outputArray[index] = el;
+            index += 1;
+        }
+        outputArray[index] = element;
+        return outputArray;
+    }
+
+    public static int[] removeDuplicates(int[] integers) {
+
+        int[] integersWithoutDuplicates = new int[0];
+            for (int el : integers) {
+
+                if (integerInArrayCount(el, integers) == 1) {
+                    integersWithoutDuplicates = addNewSlotWithElToArray(el, integersWithoutDuplicates);
+                }
+                if (integerInArrayCount(el, integers) > 1 && integerInArrayCount(el, integersWithoutDuplicates) == 0) {
+                    integersWithoutDuplicates = addNewSlotWithElToArray(el, integersWithoutDuplicates);
+                }
+            }
+        return integersWithoutDuplicates;
+
+    }
+
+    public static int sumIgnoringDuplicates(int[] integers) {
+        int sumOfElements = 0;
+        int[] arrayWithoutDuplicates = removeDuplicates(integers);
+        for (int value : arrayWithoutDuplicates) {
+            sumOfElements += value;
+        }
+        return sumOfElements;
+    }
+
+}
---
+++ src/junit/Tests.java
@@ -0,0 +1,95 @@
+package junit;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
+
+@SuppressWarnings("PMD")
+public class Tests {
+
+    @Test
+    public void equalityExamples() {
+        assertTrue(1 == 1);
+        assertFalse(1 == 2);
+
+        Integer x2 = 128;
+        Integer y2 = 128;
+        assertTrue(x2 == y2);
+
+    }
+
+    @Test
+    public void assertThatAndAssertEqualsExample() {
+        assertEquals(1 + 2, 3);
+        assertEquals(1 + 2, 4);
+        assertThat(1 + 2, is(3));
+    }
+
+    @Test
+    public void findsSpecialNumbers() {
+        assertThat(Code.isSpecial(0), is(true));
+        assertThat(Code.isSpecial(1), is(true));
+        assertThat(Code.isSpecial(2), is(true));
+        assertThat(Code.isSpecial(3), is(true));
+        assertThat(Code.isSpecial(4), is(false));
+        assertThat(Code.isSpecial(11), is(true));
+        assertThat(Code.isSpecial(15), is(false));
+        assertThat(Code.isSpecial(36), is(true));
+        assertThat(Code.isSpecial(37), is(false));
+
+        // other test cases for isSpecial() method
+    }
+
+    @Test
+    public void findsLongestStreak() {
+        assertThat(Code.longestStreak(""), is(0));
+        assertThat(Code.longestStreak("a"), is(1));
+        assertThat(Code.longestStreak("abbcccaaaad"), is(4));
+
+        assertThat(Code.longestStreak("aaaaaaaaa"), is(9));
+        // other test cases for longestStreak() method
+    }
+
+    @Test
+    public void findsModeFromCharactersInString() {
+
+        assertThat(Code.mode(null), is(nullValue()));
+        assertThat(Code.mode(""), is(nullValue()));
+        assertThat(Code.mode("abcb"), is('b'));
+        assertThat(Code.mode("cbbc"), is('c'));
+
+        // other test cases for mode() method
+        assertThat(Code.getCharacterCount("cbbc",'c'), is(2));
+        assertThat(Code.getCharacterCount("cbbcccc",'c'), is(5));
+        assertThat(Code.getCharacterCount("cbbaaggelcddcc",'c'), is(4));
+    }
+
+    @Test
+    public void removesDuplicates() {
+        assertThat(Code.removeDuplicates(arrayOf(1, 1)), is(arrayOf(1)));
+
+        assertThat(Code.removeDuplicates(arrayOf(1, 2, 1, 3, 2)), is(arrayOf(1, 2, 3)));
+
+        assertThat(Code.removeDuplicates(arrayOf(1, 2, 3)), is(arrayOf(1, 2, 3)));
+
+        assertThat(Code.removeDuplicates(arrayOf(100, 0, 3, 100, 0, 4, 562, 4)),
+                is(arrayOf(100, 0, 3, 4, 562)));
+    }
+
+    @Test
+    public void sumsIgnoringDuplicates() {
+        assertThat(Code.sumIgnoringDuplicates(arrayOf(1, 1)), is(1));
+
+        assertThat(Code.sumIgnoringDuplicates(arrayOf(1, 2, 1, 3, 2)), is(6));
+
+        assertThat(Code.sumIgnoringDuplicates(arrayOf(1, 2, 3)), is(6));
+    }
+
+    private int[] arrayOf(int... numbers) {
+        return numbers;
+    }
+
+}
